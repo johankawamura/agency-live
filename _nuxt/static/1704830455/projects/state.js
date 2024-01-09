e2d2d09bdbb5d89f0cc925b9747395bc5895598a
Johan Kawamura <johan@kawamura.de>
2024-01-09 21:13:25 +0100

    DSAd

--- _nuxt/static/1704830455/projects/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fagency-live\u002F_nuxt\u002Fstatic\u002F1704830455",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002Fagency-live\u002F",assetsPath:"\u002Fagency-live\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
